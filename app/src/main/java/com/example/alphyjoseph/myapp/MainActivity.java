3f4dfe63db97ca39fe003ef692668216d3935f05
Devipriya Sarkar <devipriyasarkar96@gmail.com>
2016-09-26 20:54:34 +0530

    Update MainActivity,java

--- app/src/main/java/com/example/alphyjoseph/myapp/MainActivity.java
+++ app/src/main/java/com/example/alphyjoseph/myapp/MainActivity.java
@@ -31,7 +31,7 @@ public class MainActivity extends AppCompatActivity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.content_main);
+        setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
